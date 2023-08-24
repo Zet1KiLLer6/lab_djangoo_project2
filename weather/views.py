50a94896b5ab8e8527bb8f7f279758157f47b6ee
davud <aitievnurgazy@gmail.com>
2023-08-25 02:12:43 +0600

    almost

--- weather/views.py
+++ weather/views.py
@@ -1,17 +1,26 @@
 import requests
 from django.shortcuts import render
 from .models import City
+from .forms import CityForm
 
 def index(request):
     appid = 'a577d2001cffb2ae7dfe9460ef6bdc9d'
     url = 'https://api.openweathermap.org/data/2.5/weather?q=Bishkek&units=metric&appid=' + appid   #https://api.openweathermap.org/data/2.5/weather?q=Bishkek&units=metric&appid=a577d2001cffb2ae7dfe9460ef6bdc9d
+
+    if(request.method=="POST"):
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
 
     cities = City.objects.all()
 
     all_cities = []
 
     for city in cities:
-        res = requests.get(url.format(city.name)).json()
+        city_url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}'.format(city.name, appid)
+        res = requests.get(city_url).json()
+
         city_info = {
             'city': city.name,
             'temp': res['main']['temp'],
@@ -20,7 +29,6 @@ def index(request):
 
         all_cities.append(city_info)
 
-
-    context = {'all_info': all_cities}
+    context = {'all_info': all_cities, "form": form}
 
     return render(request, 'weather/index.html', context)
